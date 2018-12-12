403ae8a600c74a4637feccde782aeb5bbc8f075c
Dmitry G. Anderson <dandgerson@gmail.com>
2018-12-12 18:20:17 +0300

    finish section-3

--- app.js
+++ app.js
@@ -1,53 +1,13 @@
 'use strict';
 
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
+const routes = require('./routes');
 
-    res.write(`
-      <html>
-        <head>
-          <title>My first Page</title>
-        </head>
-        <body>
-          <form action='/message' method='POST'>
-            <input type='text' name='message'>
-            <button type='submit'>Send</button>
-          </form>
-        </body>
-      </html>`);
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', chunk => {
-      body.push(chunk);
-      console.log(chunk);
-    })
-    return req.on('end', () => {
-      const parseBody = Buffer.concat(body).toString();
-      const message = parseBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-      res.writeHead(304, {'Location': '/'});
-      return res.end();
-    });
-  }
+const server = http.createServer(routes);
 
-
-  res.setHeader('Conten-Type', 'text/html');
-  res.write('Default Server Behavior...');
-  res.end();
-});
-
-server.on('error', e => {
-  if (e) {
-    console.log(e);
-  }
+server.on('error', err => {
+  if (err) throw err;
 });
 
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    res.write(`
+      <html>
+        <head>
+          <title>My first Page</title>
+        </head>
+        <body>
+          <form action='/message' method='POST'>
+            <input type='text' name='message'>
+            <button type='submit'>Send</button>
+          </form>
+        </body>
+      </html>`);
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    
+    const body = [];
+    
+    req.on('data', chunk => {
+      body.push(chunk);
+      console.log(chunk);
+    });
+    
+    return req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split('=')[1];
+      
+      fs.writeFile('./message.txt', message, err => {
+        if (err) throw err;
+        
+        res.writeHead(304, {'Location': '/'});
+        return res.end();
+      });
+      
+    });
+  }
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  res.end('Default Server Behavior...');
+};
+
+module.exports = requestHandler;
