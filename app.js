b4d253cb2555920035001fca2bfc6338fc1b164e
Dmitry G. Anderson <dandgerson@gmail.com>
2018-12-13 16:50:04 +0300

    init section5-project && complete stage 1 & 2

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 'use strict';
 
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
-
-server.on('error', err => {
-  if (err) throw err;
+app.use('/add-product', (req, res, next) => {
+  console.log('Enter in the another middleware');
+  res.send(`
+    <h1>Add Product Here</h1>`);
 });
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+  console.log('Enter in the another middleware');
+  res.send(`
+    <h1>Hello from Express</h1>`);
+});
+
+app.listen(3000);
---
+++ section5-project/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('In the Middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In the another Middleware');
+  res.send(`
+    <h1>Hello from Express</h1>`);
+});
+
+app.listen(3000);
