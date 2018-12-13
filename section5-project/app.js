4ee117789c9e999f971e7648368ac3c1434e14f9
Dmitry G. Anderson <dandgerson@gmail.com>
2018-12-13 17:00:04 +0300

    complete stage3

--- section5-project/app.js
+++ section5-project/app.js
@@ -4,13 +4,18 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the Middleware');
-  next();
+app.use('/users', (req, res, next) => {
+  res.send(`
+    <h1>Userlist Page</h1>
+    <ul>
+      <li>User 1</li>
+      <li>User 2</li>
+      <li>User 3</li>
+      <li>User 4</li>
+    </ul>`);
 });
 
-app.use((req, res, next) => {
-  console.log('In the another Middleware');
+app.use('/', (req, res, next) => {
   res.send(`
     <h1>Hello from Express</h1>`);
 });
