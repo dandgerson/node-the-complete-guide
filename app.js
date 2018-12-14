45b51dad448f9ac97c025478f6162bbb62d5939a
Dmitry G. Anderson <dandgerson@gmail.com>
2018-12-14 16:31:11 +0300

    complete pug & handlebars implantation

--- app.js
+++ app.js
@@ -4,22 +4,32 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const app = express();
+const expressHbs = require('express-handlebars');
 
 const rootDir = require('./util/path');
 
-const adminRoutes = require('./routes/admin');
+const app = express();
+
+app.engine('hbs', expressHbs({
+  layoutsDir: path.join(rootDir, 'views', 'layouts'),
+  defaultLayout: 'main-layout',
+  extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
