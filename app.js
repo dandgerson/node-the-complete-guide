75564db1c56edd49c2b68b36c72cf1963ba79d8d
Dmitry G. Anderson <dandgerson@gmail.com>
2018-12-15 14:49:58 +0300

    init section 7

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
-'use strict';
-
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const rootDir = require('./util/path');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(rootDir, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {
-    pageTitle: 'Page Not Found',
-    path: false
-  });
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
