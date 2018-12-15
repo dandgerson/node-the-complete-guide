3520c01a17e10ae589be4ca4fa461372f540d401
Dmitry G. Anderson <dandgerson@gmail.com>
2018-12-15 15:56:21 +0300

    add MVC pattern > add controllers & edit styles

---
+++ controllers/error.js
@@ -0,0 +1,8 @@
+'use strict';
+
+exports.get404 = (req, res, next) => {
+  res.status(404).render('404', {
+    pageTitle: 'Page Not Found',
+    path: false,
+  });
+};
---
+++ controllers/products.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  products.push({
+    title: req.body.title,
+    path: '/admin/add-product'
+  });
+  res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+}
