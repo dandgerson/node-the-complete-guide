63c3a667a537d1edf0e2fe2f30a85512856ae2de
Dmitry G. Anderson <dandgerson@gmail.com>
2018-12-15 15:11:04 +0300

    fix bugs && edit styles

--- routes/admin.js
+++ routes/admin.js
@@ -21,7 +21,10 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
+  products.push({
+    title: req.body.title,
+    path: '/admin/add-product'
+  });
   res.redirect('/');
 });
 
