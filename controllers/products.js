3c3d427361881d225e07323d57776da0794de97f
Dmitry G. Anderson <dandgerson@gmail.com>
2018-12-26 13:13:11 +0300

    finish section-7

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
@@ -13,20 +13,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({
-    title: req.body.title,
-    path: '/admin/add-product'
-  });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
   });
-}
+};
---
+++ models/product.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = callback => {
+  fs.readFile(p, (err, data) => {
+    if (err) return callback([]);
+    callback(JSON.parse(data));
+  });
+};
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        // eslint-disable-next-line no-console
+        console.log('save error', err);
+      });
+    });
+  }
+  
+  static fetchAll(callback) {
+    getProductsFromFile(callback);
+  }
+};
