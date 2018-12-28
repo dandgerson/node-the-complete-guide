bd130918c31dd40b7e97c976d623ca9b6478b0af
Dmitry G. Anderson <dandgerson@gmail.com>
2018-12-28 12:39:18 +0300

    passing data with post request

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,11 +11,12 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product({
+    title: req.body.title,
+    imageUrl: req.body.imageUrl,
+    description: req.body.description,
+    price: req.body.price,
+  });
   product.save();
   res.redirect('/');
 };
--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 const express = require('express');
 
 const shopController = require('../controllers/shop');
@@ -13,6 +11,8 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
 
 router.get('/orders', shopController.getOrders);
 
